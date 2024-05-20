021ad0aef68e7fbb939867f74fc1ffd672fa537a
Mirko Schiavone <mirko@giffgaff.co.uk>
2024-05-20 16:35:51 +0100

    Refactored model_predict

--- model_predict.py
+++ model_predict.py
@@ -7,7 +7,22 @@ import nltk
 
 nltk.download('punkt')
 
+# Constants
+MODEL_CHECKPOINT = 'dmis-lab/biobert-v1.1'
+MODEL_PATH = 'models/biobert_postags_cased__e_16.pt'
+MAX_LENGTH = 512
+PAD_TOKEN = '<pad>'
+
+# Define POS and NER tags vocabulary
+POS_TAGS = {
+    'ADV': 0, 'VERB': 1, 'SCONJ': 2, 'PRON': 3, 'PROPN': 4, 'NOUN': 5, 'ADP': 6,
+    'ADJ': 7, 'CCONJ': 8, 'SYM': 9, 'NUM': 10, 'PART': 11, 'PUNCT': 12, 'DET': 13,
+    'INTJ': 14, 'AUX': 15, 'X': 16
+}
+NER_TAGS = {0: 'B-O', 1: 'B-AC', 2: 'B-LF', 3: 'I-LF'}
+
 class BioBertPosTagsClassifier(nn.Module):
+    """BERT model with POS tags classifier."""
     def __init__(self, output_dim, pos_vocab_size, pos_embedding_dim):
         super(BioBertPosTagsClassifier, self).__init__()
         self.model_label = 'biobert_postags_cased'
@@ -45,75 +60,36 @@ class BioBertPosTagsClassifier(nn.Module):
 
         return logits
 
-def model_predict(tokenize_text, pos_tags, device='cpu'):
-    """
-    Make a prediction from tokenized text and POS tags using the given model.
 
-    Args:
-    tokenize_text (list of str): List of tokens.
-    pos_tags (list of str): List of POS tags corresponding to each token.
-    model (BioBertPosTagsClassifier): The trained model.
-    tokenizer (transformers.PreTrainedTokenizer): Tokenizer used during model training.
-    pos_tag_vocab (dict): Dictionary mapping POS tag strings to indices.
-    device (str): Device to run the model ('cpu' or 'cuda').
+def load_model(device='cpu'):
+    """Load model."""
+    model = BioBertPosTagsClassifier(output_dim=len(NER_TAGS), pos_vocab_size=len(POS_TAGS)+1, pos_embedding_dim=16)
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device(device)))
+    model.to(device).eval()
+    return model
 
-    Returns:
-    torch.Tensor: The logits returned by the model.
-    """
-    # Load model
-    output_dim = 4
-    pos_vocab_size = 18
-    pos_embedding_dim = 16
 
-    model = BioBertPosTagsClassifier(output_dim=output_dim,
-                                     pos_vocab_size=pos_vocab_size,
-                                     pos_embedding_dim=pos_embedding_dim)
+def load_tokenizer():
+    """Load tokenizer."""
+    return BertTokenizerFast.from_pretrained(MODEL_CHECKPOINT)
 
-    model.load_state_dict(torch.load('models/biobert_postags_cased__e_16.pt',
-                                     map_location=torch.device('cpu')))
 
-    model = model.to(device)
-    model.eval()
+def encode_tokens(text):
+    """Encode tokens to tensor."""
+    tokenizer = load_tokenizer()
+    return tokenizer(text,
+                     is_split_into_words=True,
+                     padding='max_length',
+                     truncation=True,
+                     max_length=MAX_LENGTH,
+                     return_attention_mask=True,
+                     return_tensors='pt')
 
-    # Load tokenizer
-    check_point = 'dmis-lab/biobert-v1.1'
-    tokenizer = BertTokenizerFast.from_pretrained(check_point)
 
-    # Define the NER tag vocabulary
-    #ner_tag_to_ix = {'B-O': 0, 'B-AC': 1, 'B-LF': 2, 'I-LF': 3}
-    ix_to_ner_tag = {0: 'B-O', 1: 'B-AC', 2: 'B-LF', 3: 'I-LF'}
+def encode_pos_tags(pos_tags, encoded_tokens):
+    """Encode POS tags to tensor."""
+    pos_tag_vocab = vocab(POS_TAGS, min_freq=0, specials=(PAD_TOKEN,), special_first=True)
 
-    # Define the POS tag vocabulary
-    PAD_TOKEN = '<pad>'
-    pos_tag_to_ix = {'ADV': 0,
-                     'VERB': 1,
-                     'SCONJ': 2,
-                     'PRON': 3,
-                     'PROPN': 4,
-                     'NOUN': 5,
-                     'ADP': 6,
-                     'ADJ': 7,
-                     'CCONJ': 8,
-                     'SYM': 9,
-                     'NUM': 10,
-                     'PART': 11,
-                     'PUNCT': 12,
-                     'DET': 13,
-                     'INTJ': 14,
-                     'AUX': 15,
-                     'X': 16}
-    pos_tag_vocab = vocab(pos_tag_to_ix, min_freq=0, specials=(PAD_TOKEN,), special_first=True)
-
-    # Tokenize the input text
-    encoded_tokens = tokenizer(tokenize_text,
-                               is_split_into_words=True,
-                               padding='max_length',
-                               truncation=True,
-                               max_length=512,
-                               return_attention_mask=True,
-                               return_tensors='pt')
-
-    # Convert pos_tags using the vocabulary
     pos_tags_idx = [pos_tag_vocab[tag] for tag in pos_tags]
 
     encoded_pos_tags = np.ones(len(encoded_tokens['input_ids'][0]), dtype=int) * -100
@@ -122,30 +98,45 @@ def model_predict(tokenize_text, pos_tags, device='cpu'):
 
     prev_word_idx = None
     for i, word_idx in enumerate(token_ids_word_mapping):
-        # Check if the token was in the orginal sequence (otherwise will keep the -100)
+        # Check if the token was in the original sequence (otherwise will keep the -100)
         if (word_idx is not None) and (word_idx != prev_word_idx):
-            # Assign to each token (or subtoken) the same label associated to the token
+            # Assign to each token (or sub token) the same label associated to the token
             # in the original sentence
             encoded_pos_tags[i] = pos_tags_idx[word_idx]
         prev_word_idx = word_idx
 
+    return torch.tensor(encoded_pos_tags, dtype=torch.long).unsqueeze(0)
+
+
+def logits_to_ner_tags(logits, encoded_pos_tags_tensor):
+    """Convert model logits to NER tags."""
+    _, predicted_ner_tags = torch.max(logits, dim=2)
+    active_accuracy = (encoded_pos_tags_tensor != -100).cpu()
+    predicted_ner_tags = torch.masked_select(predicted_ner_tags.cpu(), active_accuracy).cpu().numpy()
+
+    return [NER_TAGS[p] for p in predicted_ner_tags]
+
+def model_predict(tokenize_text, pos_tags, device='cpu'):
+    """Predict NER tags for a given text."""
+    # Load model
+    model = load_model(device)
+
+    # Encode tokens
+    encoded_tokens = encode_tokens(tokenize_text)
+
+    # Encode POS tags
+    encoded_pos_tags = encode_pos_tags(pos_tags, encoded_tokens)
+
     # Convert to tensor
-    token_ids = encoded_tokens['input_ids'].to(device)
-    attention_mask = encoded_tokens['attention_mask'].to(device)
-    pos_tags_tensor = torch.tensor(encoded_pos_tags, dtype=torch.long).unsqueeze(0).to(device)
+    encoded_tokens_tensor = encoded_tokens['input_ids'].to(device)
+    attention_mask_tensor = encoded_tokens['attention_mask'].to(device)
+    encoded_pos_tags_tensor = encoded_pos_tags.to(device)
 
     # Forward pass through the model
     with torch.no_grad():
-        logits = model(token_ids, attention_mask, pos_tags_tensor)
+        logits = model(encoded_tokens_tensor, attention_mask_tensor, encoded_pos_tags_tensor)
 
-        # Get the predictions
-        _, predicted_ner_tags = torch.max(logits, dim=2)
-
-        # Update lists of true and predicted tags, excluding ignored index (-100 used for padding in BERT)
-        active_accuracy = (pos_tags_tensor != -100).cpu()
-        predicted_ner_tags = torch.masked_select(predicted_ner_tags.cpu(), active_accuracy).cpu().numpy()
-
-    return [ix_to_ner_tag[p] for p in predicted_ner_tags]
+    return logits_to_ner_tags(logits, encoded_pos_tags_tensor)
 
 # Example usage
 # Assume model, tokenizer, and pos_tag_vocab are already defined and loaded
